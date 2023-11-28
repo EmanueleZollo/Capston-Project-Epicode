fe61ae5f047bc63fc6ac67980c8864d0898a0604
Emanuele Zollo - NjS Studio <ez.studionjs@gmail.com>
2023-11-28 16:17:23 +0100

    first commit

---
+++ src/main/java/UnitedGymsProject/GymBrofriendly/GymBroFriendlyApplication.java
@@ -0,0 +1,13 @@
+package UnitedGymsProject.GymBrofriendly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymBroFriendlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymBroFriendlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/UnitedGymsProject/GymBrofriendly/GymBroFriendlyApplicationTests.java
@@ -0,0 +1,13 @@
+package UnitedGymsProject.GymBrofriendly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymBroFriendlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
